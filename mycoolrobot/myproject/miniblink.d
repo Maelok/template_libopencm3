faa24496b30fe0cbef8ba22a5884a52de3b8da81
Maelok <maelok.dong@gmail.com>
2014-07-28 09:50:05 +0800

    Add JLink Support

---
+++ mycoolrobot/myproject/miniblink.d
@@ -0,0 +1,19 @@
+miniblink.o: miniblink.c ../libopencm3/include/libopencm3/stm32/rcc.h \
+ ../libopencm3/include/libopencm3/cm3/common.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdint.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/stdint.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/_ansi.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/newlib.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/config.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/../../../../arm-none-eabi/include/sys/features.h \
+ /home/maelok/gcc-arm-none-eabi-4_7-2014q2/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f3/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f3/rcc.h \
+ ../libopencm3/include/libopencm3/stm32/common/rcc_common_all.h \
+ ../libopencm3/include/libopencm3/stm32/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/f3/gpio.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
